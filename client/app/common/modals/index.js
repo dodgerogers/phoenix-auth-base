9faeb9ab9c3043551f59f4da6d0950d81cd542c3
Andy <dodgerogers@hotmail.com>
2017-10-22 10:06:36 -0400

    fix import in modal reducer

--- client/app/common/modals/index.js
+++ client/app/common/modals/index.js
@@ -1,9 +1,11 @@
 import { modalIds as ModalIds } from './constants';
 import * as ModalActions from './actions';
 import ModalTrigger from './components/ModalTrigger';
+import Reducer from './reducer';
 
 export {
   ModalActions,
   ModalIds,
   ModalTrigger,
+  Reducer,
 };
