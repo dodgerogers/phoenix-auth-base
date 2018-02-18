14ce3338d2d2457f3418df782da9c208b80002b0
dodgerogers <dodgerogers@hotmail.com>
2018-02-18 11:26:46 -0500

    HTTP tests, fix input errors

--- client/app/lib/utils/HTTP.js
+++ client/app/lib/utils/HTTP.js
@@ -5,20 +5,20 @@ import { currentAccessTokenValue, isTokenRefreshing } from '../../authentication
 import wait from './wait';
 
 
-const localhost = 'http://localhost';
-export const API_BASE = process.env.NODE_ENV === 'test' ? localhost : window.location.origin;
+const testUrl = 'http://localhost';
+const API_BASE = process.env.NODE_ENV === 'test' ? testUrl : window.location.origin;
 const HTTP = axios.create({ baseURL: API_BASE });
 
-HTTP.interceptors.request.use(requestInterceptor, function(error) {
+HTTP.interceptors.request.use(requestInterceptor, error => Promise.reject(error));
+HTTP.interceptors.response.use(config => { // TODO: Handle 401
+  config.data = normalize(config.data);
+  return config;
+}, error => {
+  error.response.data = normalize(error.response.data);
   return Promise.reject(error);
 });
 
-HTTP.interceptors.response.use(response => { // TODO: Handle 401
-  response.data = normalize(response.data);
-  return response;
-}, function(error) {
-  return Promise.reject(error);
-});
+const normalizeKeys = data => normalize(data, 'snake');
 
 async function requestInterceptor(config) {
   return waitForAccessToken(config.url)
@@ -36,9 +36,7 @@ async function waitForAccessToken(url) {
   }
 }
 
-function isTokenRefreshRequest(url) {
-  return url && url.indexOf('/oauth/token/refresh') > -1;
-}
+const isTokenRefreshRequest = url => url && url.indexOf('/oauth/token/refresh') > -1;
 
 function transformRequest(config, accessToken) {
   if (accessToken) {
